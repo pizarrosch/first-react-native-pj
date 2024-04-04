21569d51071c2e16b8bdd0b3ac3173140bd27b06
pizarrosch <bayenrshc89@gmail.com>
2024-04-05 00:20:11 +0200

    Add some responsive styling while deleting an item

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,22 +2,32 @@ import {Text, View, StyleSheet, Pressable} from "react-native";
 
 export default function GoalItem({text, onDeleteItem, goalItemId}) {
   return (
-    <Pressable onPress={onDeleteItem.bind(this, goalItemId)}>
-      <View style={styles.goalListItem}>
-        {/*ItemDataObj is an object with built-in properties, like, for example, an "item" property*/}
-        <Text>{text}</Text>
-      </View>
-    </Pressable>
+    <View style={styles.goalListItem}>
+      {/*ItemDataObj is an object with built-in properties, like, for example, an "item" property*/}
+      <Pressable
+        onPress={onDeleteItem.bind(this, goalItemId)}
+        style={({pressed}) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{text}</Text>
+      </Pressable>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   goalListItem: {
     margin: 7,
-    padding: 7,
     borderRadius: 6,
-    backgroundColor: '#EDEFF2',
-    borderBottomWidth: 3,
-    borderBottomColor: '#D3D8DE'
+    backgroundColor: '#4C90F0',
+    shadowColor: '#8ABBFF',
+    shadowOffset: {width: 0, height: 5},
+    shadowOpacity: 15
+  },
+  pressedItem: {
+    backgroundColor: '#215DB0',
+    borderRadius: 6
+  },
+  goalText: {
+    padding: 7,
   }
 })
